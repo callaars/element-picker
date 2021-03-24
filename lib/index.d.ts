c6e674ef4f9541669d2bbe393fdf1e702c92356b
Joseph Callaars <joseph@callaa.rs>
2021-03-24 18:29:41 +0000

    :green_heart: Remove build directory

--- lib/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare type StartOptions = {
-    onClick: (element: Element) => void;
-    hoverStyle: any;
-};
-declare const _default: {
-    start: (options: StartOptions) => void;
-};
-export default _default;
