aeec6c24c3679a2ca9402478da613d15bd487e40
Joseph Callaars <joseph@callaa.rs>
2021-03-24 18:04:30 +0000

    :tada: initial commit.

---
+++ src/index.ts
@@ -0,0 +1,79 @@
+type StartOptions = {
+  onClick: (element: Element) => void;
+  hoverStyle: any;
+};
+
+let onClick = (_: any) => {};
+let applyStyle: Partial<CSSStyleDeclaration> = {};
+
+let lastTarget: HTMLElement | null = null;
+let lastTargetStyle: Partial<CSSStyleDeclaration> = {};
+
+const reset = () => {
+  document.body.style.cursor = 'auto';
+
+  document.removeEventListener('click', onMouseClick, false);
+  document.removeEventListener('mousemove', onMouseClick, false);
+
+  removeTargetStyle();
+
+  applyStyle = {};
+  onClick = (_: any) => {};
+
+  lastTarget = null;
+  lastTargetStyle = {};
+};
+
+const removeTargetStyle = () => {
+  if (lastTarget) {
+    Object.keys(applyStyle).forEach((key: any) => {
+      delete lastTarget?.style[key as any];
+
+      if (lastTargetStyle[key]) {
+        lastTarget!.style[key] = lastTargetStyle[key]!;
+      }
+    });
+  }
+};
+
+const applyTargetStyle = (target: any) => {
+  Object.keys(applyStyle).forEach((key: any) => {
+    lastTargetStyle[key] = target.style[key];
+    target.style[key] = applyStyle[key];
+  });
+};
+
+const onMouseClick = (event: Event) => {
+  event.preventDefault && event.preventDefault();
+  event.stopPropagation && event.stopPropagation();
+
+  onClick(event.target);
+  reset();
+};
+
+const onMouseMove = (event: Event) => {
+  removeTargetStyle();
+
+  lastTarget = event.target as HTMLElement;
+
+  applyTargetStyle(lastTarget);
+};
+
+const start = (options: StartOptions) => {
+  reset();
+
+  if (options.onClick) {
+    onClick = options.onClick;
+  }
+
+  if (options.hoverStyle) {
+    applyStyle = {};
+  }
+
+  document.body.style.cursor = 'pointer';
+
+  document.addEventListener('click', onMouseClick, false);
+  document.addEventListener('mousemove', onMouseMove, false);
+};
+
+export default { start };
